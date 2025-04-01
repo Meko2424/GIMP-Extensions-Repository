82b908986a6e7c79258734a3ab01ea6daa03e43d
mekonnen <mekonnen.cs@gmail.com>
2025-04-01 18:31:07 -0400

    Initial project setup

---
+++ src/main/java/com/mk/GIMP/Extension/Repository/GimpExtensionRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.mk.GIMP.Extension.Repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimpExtensionRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimpExtensionRepositoryApplication.class, args);
+	}
+
+}
